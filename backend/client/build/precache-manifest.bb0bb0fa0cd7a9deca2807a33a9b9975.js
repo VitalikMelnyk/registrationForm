84cac27513a9e1dd8cdcab0f0015b4e1c7e005c3
Vitalik Melnyk <melnyk.vitalikk@gmail.com>
2020-02-21 14:17:04 +0200

    change location of client

---
+++ backend/client/build/precache-manifest.bb0bb0fa0cd7a9deca2807a33a9b9975.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be2932a0d2a772f5795c23ccdcb2a4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4720fd404718841fa6",
+    "url": "/static/css/2.51f4a81e.chunk.css"
+  },
+  {
+    "revision": "c4a54c6c85af666af875",
+    "url": "/static/css/main.26ffe7c9.chunk.css"
+  },
+  {
+    "revision": "6e4720fd404718841fa6",
+    "url": "/static/js/2.29ef858a.chunk.js"
+  },
+  {
+    "revision": "5235bb834f9e080d13ef560bd5516cc7",
+    "url": "/static/js/2.29ef858a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4a54c6c85af666af875",
+    "url": "/static/js/main.e2dc0099.chunk.js"
+  },
+  {
+    "revision": "681b084cb042beec214d",
+    "url": "/static/js/runtime-main.687129d8.js"
+  },
+  {
+    "revision": "74c88a01d2b90268f10d2f87e9d79cbc",
+    "url": "/static/media/trianglify-lowres.74c88a01.png"
+  }
+]);
